9bb7bf7ccdb72bb44f20ff26b865baa0379bc43e
Ksenia51 <Tesa11234455666t@yandex.ru>
2024-05-01 11:07:50 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, ()=>{
+    console.log('Server has been start on http://localhost:3000');
+});
