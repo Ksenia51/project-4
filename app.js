61dcb03ae1b0b810ca1ef650e86bd2f1e69cf1f1
Ksenia51 <Tesa11234455666t@yandex.ru>
2024-05-02 13:30:21 +0300

    feat{route/games.js}: added placeholders for POST and DELETE methods

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const fs = require("fs").promises;
 const path = require("path");
+const mainRouter = require("./routes/main");
+const gamesRouter = require("./routes/games");
 
 const app = express();
 
-const PORT = 3001;
+const PORT = 3002;
 
 app.listen(PORT, ()=>{
-    console.log(`Server has been started on http://locallhost:3001`);
+    console.log(`Server has been started on http://locallhost:3002`);
 });
 
 app.use(express.static(path.join(__dirname, "public")), 
-gamesRouter, 
-mainRouter
+mainRouter,
+gamesRouter
 );
