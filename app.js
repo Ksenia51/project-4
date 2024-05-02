2531e01d05b316e83d3c78823295f7b2df80b5f2
Ksenia51 <Tesa11234455666t@yandex.ru>
2024-05-02 12:36:42 +0300

    feat: added frontend

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require("express");
+const fs = require("fs").promises;
+const path = require("path");
 
 const app = express();
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.listen(PORT, ()=>{
-    console.log('Server has been start on http://localhost:3000');
+    console.log(`Server has been started on http://locallhost:3001`);
 });
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res)=>{
-    res.send("Hello");
+    fs.readFile("./public/index.html", "utf-8").then((data)=>{
+        res.header("Content-Type", "text/html");
+        res.send(data);
+    });
 });
