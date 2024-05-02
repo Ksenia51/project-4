7d9778b4d185960ed87d1cc9a7596ba93a049880
Ksenia51 <Tesa11234455666t@yandex.ru>
2024-05-02 13:04:23 +0300

    feat: added main and games routers

--- app.js
+++ app.js
@@ -10,11 +10,7 @@ app.listen(PORT, ()=>{
     console.log(`Server has been started on http://locallhost:3001`);
 });
 
-app.use(express.static(path.join(__dirname, "public")));
-
-app.get("/", (req, res)=>{
-    fs.readFile("./public/index.html", "utf-8").then((data)=>{
-        res.header("Content-Type", "text/html");
-        res.send(data);
-    });
-});
+app.use(express.static(path.join(__dirname, "public")), 
+gamesRouter, 
+mainRouter
+);
---
+++ routes/main.js
@@ -0,0 +1,12 @@
+const mainRouter = require("express").Router();
+
+const sendHtml = (req, res)=>{
+    fs.readFile("./public/index.html", "utf-8").then((data)=>{
+        res.header("Content-Type", "text/html");
+        res.send(data);
+    });
+};
+
+mainRouter.get("/", sendHtml);
+
+module.exports = mainRouter;
